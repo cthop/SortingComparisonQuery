3c6ec11d23eba1f49538f6ec22fd8e8472d28233
Christopher T <chritopherjthomaswork@gmail.com>
2023-12-25 03:06:54 +1300

    Updated logic

--- merge_sort.py
+++ merge_sort.py
@@ -1,11 +1,11 @@
-def merge_sort(arr):
+def merge_sort_not_in_place(arr):
     if len(arr) > 1:
         mid = len(arr) // 2
         L = arr[:mid]
         R = arr[mid:]
 
-        merge_sort(L)
-        merge_sort(R)
+        merge_sort_not_in_place(L)
+        merge_sort_not_in_place(R)
 
         merge(arr, L, R)
 
